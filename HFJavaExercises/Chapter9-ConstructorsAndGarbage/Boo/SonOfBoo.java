5dd48894f723601b35acc880a157bf1735d6c7f1
Jackman <brianjackman@live.ie>
2024-02-22 19:19:39 +0000

    Adding work and exercises completed from chapters 8 and 9

---
+++ HFJavaExercises/Chapter9-ConstructorsAndGarbage/Boo/SonOfBoo.java
@@ -0,0 +1,18 @@
+class SonOfBoo extends Boo {
+	
+	public SonOfBoo() {
+		super("boo");
+	}
+	
+	public SonOfBoo(int i) {
+		super("Fred");
+	}
+	
+	public SonOfBoo(String s) {
+		super(42);
+	}
+	
+//	public SonOfBoo(int i, String s) {
+//	}
+	
+}
